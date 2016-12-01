c2ed7c7c19aa41109c98ae14b10cb0ea4f535a42
Freddy <freddy.buitenwerf@gmail.com>
2016-12-01 16:02:43 +0100

    Update geupdatet

--- app/http/Controllers/UserController.php
+++ app/http/Controllers/UserController.php
@@ -57,8 +57,30 @@ class UserController extends Controller
     public function update(Request $req)
     {
         $user = Auth::user();
-        $studentnummer = $req->input('studentnummer');
-        $user->studentnummer = $studentnummer;
+        $new_user_data = array(
+            'voornaam' => $req['voornaam'],
+            'tussenvoegsel' => $req['tussenvoegsel'],
+            'achternaam' => $req['achternaam'],
+            'geslacht' => $req['geslacht'],
+            'email' => $req['email'],
+            'nationaliteit' => $req['nationaliteit'],
+            'geboorteland' => $req['geboorteland'],
+            'geboorteprovincie' => $req['geboorteprovincie'],
+            'geboorteplaats' => $req['geboorteplaats'],
+            'geboortedatum' => $req['geboortedatum'],
+            'titel' => $req['titel'],
+            'burgerlijke_staat' => $req['burgerlijke_staat'],
+            'heeft_kinderen' => $req['heeft_kinderen'],
+            'jaarinkomen' => $req['jaarinkomen'],
+            'post_adres' => $req['post_adres'],
+            'studentnummer' => $req['studentnummer'],
+            'telefoonnummer' => $req['telefoonnummer'],
+            'facebook' => $req['facebook'],
+            'linkedin' => $req['linkedin'],
+            'twitter' => $req['twitter'],
+            'website' => $req['website']
+        );
+        $user->fill($new_user_data);
         $user->save();
         return view('profiel', array('user' => Auth::user() ) );
     }
