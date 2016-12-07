dd4e34407ece4882b0493d13dffaf2fc9d1b0584
RubenWeppner <rweppner3@gmail.com>
2016-12-07 11:04:10 +0100

    Re-added semi-realistic seeders.

---
+++ database/seeds/add_real_opleiding.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class add_real_opleiding extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('opleiding')->insert(array(
+            array('user_id'=>'1', 'naam'=>'Software Development', 'instituut'=>'Windesheim Flevoland', 'richting'=>'ICT', 'begin'=>'2014-03-19', 'eind'=>'2016-01-01', 'locatie'=>'Almere', 'niveau'=>'AD', 'behaald'=>'1', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'2', 'naam'=>'Logopedie', 'instituut'=>'Windesheim Flevoland', 'richting'=>'Health', 'begin'=>'2013-03-19', 'eind'=>'2015-01-01', 'locatie'=>'Almere', 'niveau'=>'Bachelor', 'behaald'=>'1', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'2', 'naam'=>'Bouwkunde', 'instituut'=>'Windesheim Flevoland', 'richting'=>'Technology', 'begin'=>'2010-01-23', 'eind'=>'2012-06-07', 'locatie'=>'Almere', 'niveau'=>'Bachelor', 'behaald'=>'1', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'3', 'naam'=>'Leraar Basisonderwijs', 'instituut'=>'Windesheim Flevoland', 'richting'=>'Education', 'begin'=>'2010-05-13', 'eind'=>'2014-08-24', 'locatie'=>'Almere', 'niveau'=>'Bachelor', 'behaald'=>'1', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'4', 'naam'=>'Ondernemen', 'instituut'=>'Windesheim Flevoland', 'richting'=>'Business', 'begin'=>'2012-03-19', 'eind'=>'2014-08-11', 'locatie'=>'Almere', 'niveau'=>'AD', 'behaald'=>'1', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'5', 'naam'=>'Pedagogiek', 'instituut'=>'Windesheim Flevoland', 'richting'=>'Social work', 'begin'=>'2015-06-30',  'eind'=>NULL,'locatie'=>'Almere', 'niveau'=>'Bachelor', 'behaald'=>'0', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'6', 'naam'=>'HBO-Rechten', 'instituut'=>'Windesheim Flevoland', 'richting'=>'Law', 'begin'=>'1970-03-19',  'eind'=>NULL, 'locatie'=>'Almere', 'niveau'=>'Bachelor', 'behaald'=>'0', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'7', 'naam'=>'Gymnasium', 'instituut'=>'Het Baken', 'richting'=>'N&T', 'begin'=>'2019-03-19', 'eind'=>'2045-07-21', 'locatie'=>'Almere', 'niveau'=>'VWO', 'behaald'=>'1', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'8', 'naam'=>'Applicatieontwikkelaar', 'instituut'=>'ROC Flevoland', 'richting'=>'ICT', 'begin'=>'2008-05-04', 'eind'=>'2011-01-01', 'locatie'=>'Almere', 'niveau'=>'MBO', 'behaald'=>'1', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'8', 'naam'=>'Software Development', 'instituut'=>'Windesheim Flevoland', 'richting'=>'ICT', 'begin'=>'2011-03-19', 'eind'=>'2013-04-26', 'locatie'=>'Almere', 'niveau'=>'AD', 'behaald'=>'1', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+        ));
+    }
+}
---
+++ database/seeds/add_real_woon.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class add_real_woon extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('woonplaats')->insert(array(
+            array('user_id'=>'1', 'naam'=>'Almere', 'begin'=>'2014-03-19', 'eind'=>NULL, 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'2', 'naam'=>'Zaandam', 'begin'=>'2014-03-19', 'eind'=>NULL, 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'2', 'naam'=>'Urk', 'begin'=>'2012-01-01', 'eind'=>'2014-01-01', 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'2', 'naam'=>'Almere', 'begin'=>'2000-05-15', 'eind'=>'2011-12-31', 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'2', 'naam'=>'Amsterdam', 'begin'=>'1990-01-12', 'eind'=>'2000-01-01', 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'3', 'naam'=>'Almere', 'begin'=>'2014-03-19', 'eind'=>NULL, 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'3', 'naam'=>'Beijing', 'begin'=>'2011-01-01', 'eind'=>'2013-12-31', 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'China', 'provincie'=>NULL),
+            array('user_id'=>'3', 'naam'=>'Almere', 'begin'=>'2010-06-25', 'eind'=>'2010-12-31', 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'4', 'naam'=>'Valencia', 'begin'=>'2011-01-09', 'eind'=>NULL, 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Spanje', 'provincie'=>NULL),
+            array('user_id'=>'4', 'naam'=>'Almere', 'begin'=>'2010-01-04', 'eind'=>'2011-01-01', 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+            array('user_id'=>'5', 'naam'=>'Madrid', 'begin'=>'2012-02-29', 'eind'=>NULL, 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Spanje', 'provincie'=>NULL),
+            array('user_id'=>'6', 'naam'=>'Milan', 'begin'=>'2015-01-11', 'eind'=>NULL, 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Italie', 'provincie'=>'Flevoland'),
+            array('user_id'=>'7', 'naam'=>'Maanstad', 'begin'=>'2009-03-19', 'eind'=>NULL, 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Maan', 'provincie'=>'Nieuw Flevoland'),
+            array('user_id'=>'8', 'naam'=>'Jupiterdam', 'begin'=>'2056-01-23', 'eind'=>NULL, 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Jupiter', 'provincie'=>NULL),
+            array('user_id'=>'8', 'naam'=>'Almere', 'begin'=>'2010-06-16', 'eind'=>'2045-01-24', 'longitude'=>'8.657057', 'latitude'=>'91.582031', 'Land'=>'Nederland', 'provincie'=>'Flevoland'),
+
+        ));
+    }
+}
