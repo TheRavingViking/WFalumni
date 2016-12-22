59f88fb3f540d587bd656b127885efd889df87d0
TheRavingViking <sjoerdvangalen93@gmail.com>
2016-12-22 20:39:40 +0100

    geochart

--- resources/views/geochart.blade.php
+++ resources/views/geochart.blade.php
@@ -43,9 +43,9 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" >
                     <div class="embed-responsive embed-responsive-16by9">
-                        <div id="map_canvas" class="embed-responsive-item">
+                        <div id="map_canvas" class="embed-responsive-item" style="padding: 1em">
                             {!! Mapper::render () !!}
                         </div>
                     </div>
