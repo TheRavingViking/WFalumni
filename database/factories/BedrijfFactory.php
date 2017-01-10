10d18febec9dfc1b6092f2b239c3367ef8c2a658
TheRavingViking <sjoerdvangalen93@gmail.com>
2017-01-10 12:22:59 +0100

    login redirects to correct path, bedrijfFactory

--- database/factories/BedrijfFactory.php
+++ database/factories/BedrijfFactory.php
@@ -14,9 +14,12 @@
 $factory->define(App\Bedrijf::class, function (Faker\Generator $faker) {
 
     $faker->addProvider(new Faker\Provider\nl_NL\Company($faker));
+
+    $richting = array('Business', 'ICT', 'Law');
+
     return [
         'functie' => $faker->name,
-        'richting' => $faker->name,
+        'richting' => $faker->randomElements($richting),
         'naam' => $faker->company,
         'straatnaam' => $faker->address,
         'postcode' => $faker->postcode,
