bfd8c6c0ad20ca407d82ba4a5a7c9c8604628c1e
LaurensAl <Clipsedesign@gmail.com>
2017-01-11 15:46:51 +0100

    vormgeving update profiel pagina, register(navbar) greyed out, adminopleidingen vormgeving toegevoegd

--- resources/views/adminOpleidingen.blade.php
+++ resources/views/adminOpleidingen.blade.php
@@ -4,41 +4,46 @@
 
     <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-md-offset-2">
         <div class="panel panel-default" style="padding: 1em">
-    <form method="POST" action="/adminOpleidingen/richting">
-        {!! csrf_field() !!}
-        <h2>Voeg richting toe</h2>
-        <input  type="text" name="richtingen" placeholder="Richting">
-        <button class="btn btn-primary" type="submit">Toevoegen</button>
-    </form >
-    <br /><hr>
-    <form method="POST" action="/adminOpleidingen/opleiding">
-        {!! csrf_field() !!}
-        <h2>Voeg opleiding toe</h2>
-        <select class="input-sm"name="richtingen" id="richtingen">
-            @foreach($richtingen as $richting)
-                <option value="{{ $richting->id }}">
-                    {{ $richting->naam }}
-                </option>
-            @endforeach
-        </select>
-        <input  type="text" name="opleidingen" placeholder="Opleiding">
-        <button class="btn btn-primary" type="submit">Toevoegen</button>
-    </form><br><hr>
-    <form method="POST" action="/adminOpleidingen/specialisatie">
-        {!! csrf_field() !!}
-        <h2>Voeg specialisatie toe</h2>
-        <select class="input-sm" name="opleidingen" id="opleidingen">
-            @foreach($opleidingen as $opleiding)
-                <option value="{{ $opleiding->id }}">
-                    {{ $opleiding->naam }}
-                </option>
-            @endforeach
-        </select>
-        <input type="text" name="specialisaties" placeholder="Specialisiatie">
+            <form method="POST" action="/adminOpleidingen/richting">
+                {!! csrf_field() !!}
+                <h2>Voeg richting toe</h2>
+                <input type="text" name="richtingen" placeholder="Richting">
+                <button class="btn btn-primary" type="submit">Toevoegen</button>
+            </form>
+            <br/>
+            <hr>
+            <form method="POST" action="/adminOpleidingen/opleiding">
+                {!! csrf_field() !!}
+                <h2>Voeg opleiding toe</h2>
+                <select class="input-sm" name="richtingen" id="richtingen">
+                    @foreach($richtingen as $richting)
+                        <option value="{{ $richting->id }}">
+                            {{ $richting->naam }}
+                        </option>
+                    @endforeach
+                </select>
+                <input type="text" name="opleidingen" placeholder="Opleiding">
+                <button class="btn btn-primary" type="submit">Toevoegen</button>
+            </form>
+            <br>
+            <hr>
+            <form method="POST" action="/adminOpleidingen/specialisatie">
+                {!! csrf_field() !!}
+                <h2>Voeg specialisatie toe</h2>
+                <select class="input-sm" name="opleidingen" id="opleidingen">
+                    @foreach($opleidingen as $opleiding)
+                        <option value="{{ $opleiding->id }}">
+                            {{ $opleiding->naam }}
+                        </option>
+                    @endforeach
+                </select>
+                <input type="text" name="specialisaties" placeholder="Specialisiatie">
 
-        <button class="btn btn-primary" type="submit">Toevoegen</button>
-    </form><hr>
-</div>
+                <button class="btn btn-primary" type="submit">Toevoegen</button>
+            </form>
+            <hr>
+        </div>
+    </div>
     <br>
 @endsection
 
