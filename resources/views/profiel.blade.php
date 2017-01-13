a3afe612ecd7eafa9f0ae454e3430693c1d2e45d
TheRavingViking <sjoerdvangalen93@gmail.com>
2017-01-13 13:51:31 +0100

    edit opleidingen/specialisaties/richtingen + sweetalert

--- resources/views/profiel.blade.php
+++ resources/views/profiel.blade.php
@@ -457,7 +457,7 @@
                                     </div>
 
                                     <div class="modal-body">
-                                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
 
 
                                             <form method="POST" action="{{ url('profiel/opleiding') }}"
