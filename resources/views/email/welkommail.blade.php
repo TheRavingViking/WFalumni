f0fdd80e225edd360e394b15fe779db5f45e4efa
TheRavingViking <sjoerdvangalen93@gmail.com>
2017-01-13 14:38:03 +0100

    email

--- resources/views/email/welkommail.blade.php
+++ resources/views/email/welkommail.blade.php
@@ -1,7 +1,3 @@
-U bent geregistreerd voor de Windesheim Alumni app. Klik op de onderstaande link om uw wachtwoord in te stellen:
-<a href="www.s1105718.hbo-almere.nl/setPass?auth={{ $email }}">Wachtwoord instellen</a>
-
-
 <!DOCTYPE html>
 <html>
 
