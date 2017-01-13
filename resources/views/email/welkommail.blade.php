8db0569eea9ff69c234b4245371201f2e3d3b3dd
TheRavingViking <sjoerdvangalen93@gmail.com>
2017-01-13 14:36:20 +0100

    edit opleidingen/specialisaties/richtingen + sweetalert

--- resources/views/email/welkommail.blade.php
+++ resources/views/email/welkommail.blade.php
@@ -1,24 +1,23 @@
 U bent geregistreerd voor de Windesheim Alumni app. Klik op de onderstaande link om uw wachtwoord in te stellen:
-<a href="www.s1105718.hbo-almere.nl/setPass?auth={{ $email }}" >Wachtwoord instellen</a>
+<a href="www.s1105718.hbo-almere.nl/setPass?auth={{ $email }}">Wachtwoord instellen</a>
 
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
     <style type="text/css" rel="stylesheet" media="all">
         /* Media Queries */
-        @media  only screen and (max-width: 500px) {
+        @media only screen and (max-width: 500px) {
             .button {
                 width: 100% !important;
             }
         }
     </style>
 </head>
-
 
 
 <body style="margin: 0; padding: 0; width: 100%; background-color: #F2F4F6;">
@@ -29,7 +28,8 @@ U bent geregistreerd voor de Windesheim Alumni app. Klik op de onderstaande link
                 <!-- Logo -->
                 <tr>
                     <td style="padding: 25px 0; text-align: center;">
-                        <a style="font-family: Arial, &#039;Helvetica Neue&#039;, Helvetica, sans-serif; font-size: 16px; font-weight: bold; color: #2F3133; text-decoration: none; text-shadow: 0 1px 0 white;" href="http://wfalumni.dev" target="_blank">
+                        <a style="font-family: Arial, &#039;Helvetica Neue&#039;, Helvetica, sans-serif; font-size: 16px; font-weight: bold; color: #2F3133; text-decoration: none; text-shadow: 0 1px 0 white;"
+                           href="http://wfalumni.dev" target="_blank">
                             WFalumni
                         </a>
                     </td>
@@ -37,32 +37,36 @@ U bent geregistreerd voor de Windesheim Alumni app. Klik op de onderstaande link
 
                 <!-- Email Body -->
                 <tr>
-                    <td style="width: 100%; margin: 0; padding: 0; border-top: 1px solid #EDEFF2; border-bottom: 1px solid #EDEFF2; background-color: #FFF;" width="100%">
-                        <table style="width: auto; max-width: 570px; margin: 0 auto; padding: 0;" align="center" width="570" cellpadding="0" cellspacing="0">
+                    <td style="width: 100%; margin: 0; padding: 0; border-top: 1px solid #EDEFF2; border-bottom: 1px solid #EDEFF2; background-color: #FFF;"
+                        width="100%">
+                        <table style="width: auto; max-width: 570px; margin: 0 auto; padding: 0;" align="center"
+                               width="570" cellpadding="0" cellspacing="0">
                             <tr>
                                 <td style="font-family: Arial, &#039;Helvetica Neue&#039;, Helvetica, sans-serif; padding: 35px;">
                                     <!-- Greeting -->
                                     <h1 style="margin-top: 0; color: #2F3133; font-size: 19px; font-weight: bold; text-align: left;">
-                                        Hello!
+                                        Hallo!
                                     </h1>
 
                                     <!-- Intro -->
                                     <p style="margin-top: 0; color: #74787E; font-size: 16px; line-height: 1.5em;">
-                                        You are receiving this email because we received a password reset request for your account.
+                                        Welkom je krijgt deze mail omdat je uitgenodigd bent voor de WF Alumni
+                                        applicatie
                                     </p>
 
                                     <!-- Action Button -->
-                                    <table style="width: 100%; margin: 30px auto; padding: 0; text-align: center;" align="center" width="100%" cellpadding="0" cellspacing="0">
+                                    <table style="width: 100%; margin: 30px auto; padding: 0; text-align: center;"
+                                           align="center" width="100%" cellpadding="0" cellspacing="0">
                                         <tr>
                                             <td align="center">
 
-                                                <a href="http://wfalumni.dev/password/reset/98d34989f8b29f96d53258104dc0e562bf3805292ba6f96ca71b4e0c38f7bb49"
+                                                <a href="www.s1105718.hbo-almere.nl/setPass?auth={{ $email }}"
                                                    style="font-family: Arial, &#039;Helvetica Neue&#039;, Helvetica, sans-serif; display: block; display: inline-block; width: 200px; min-height: 20px; padding: 10px;
                  background-color: #3869D4; border-radius: 3px; color: #ffffff; font-size: 15px; line-height: 25px;
                  text-align: center; text-decoration: none; -webkit-text-size-adjust: none; background-color: #3869D4;"
                                                    class="button"
                                                    target="_blank">
-                                                    Reset Password
+                                                    Set password
                                                 </a>
                                             </td>
                                         </tr>
@@ -70,12 +74,12 @@ U bent geregistreerd voor de Windesheim Alumni app. Klik op de onderstaande link
 
                                     <!-- Outro -->
                                     <p style="margin-top: 0; color: #74787E; font-size: 16px; line-height: 1.5em;">
-                                        If you did not request a password reset, no further action is required.
+                                        Als je dit niet aangevraagd hebt, kan je deze mail.
                                     </p>
 
                                     <!-- Salutation -->
                                     <p style="margin-top: 0; color: #74787E; font-size: 16px; line-height: 1.5em;">
-                                        Regards,<br>WFalumni
+                                        Groeten,<br>WFalumni
                                     </p>
 
                                     <!-- Sub Copy -->
@@ -88,8 +92,10 @@ U bent geregistreerd voor de Windesheim Alumni app. Klik op de onderstaande link
                                                 </p>
 
                                                 <p style="margin-top: 0; color: #74787E; font-size: 12px; line-height: 1.5em;">
-                                                    <a style="color: #3869D4;" href="http://wfalumni.dev/password/reset/98d34989f8b29f96d53258104dc0e562bf3805292ba6f96ca71b4e0c38f7bb49" target="_blank">
-                                                        http://wfalumni.dev/password/reset/98d34989f8b29f96d53258104dc0e562bf3805292ba6f96ca71b4e0c38f7bb49
+                                                    <a style="color: #3869D4;"
+                                                       href="www.s1105718.hbo-almere.nl/setPass?auth={{ $email }}"
+                                                       target="_blank">
+                                                        www.s1105718.hbo-almere.nl/setPass?auth={{ $email }}
                                                     </a>
                                                 </p>
                                             </td>
@@ -104,12 +110,14 @@ U bent geregistreerd voor de Windesheim Alumni app. Klik op de onderstaande link
                 <!-- Footer -->
                 <tr>
                     <td>
-                        <table style="width: auto; max-width: 570px; margin: 0 auto; padding: 0; text-align: center;" align="center" width="570" cellpadding="0" cellspacing="0">
+                        <table style="width: auto; max-width: 570px; margin: 0 auto; padding: 0; text-align: center;"
+                               align="center" width="570" cellpadding="0" cellspacing="0">
                             <tr>
                                 <td style="font-family: Arial, &#039;Helvetica Neue&#039;, Helvetica, sans-serif; color: #AEAEAE; padding: 35px; text-align: center;">
                                     <p style="margin-top: 0; color: #74787E; font-size: 12px; line-height: 1.5em;">
                                         &copy; 2017
-                                        <a style="color: #3869D4;" href="http://wfalumni.dev" target="_blank">WFalumni</a>.
+                                        <a style="color: #3869D4;" href="http://wfalumni.dev"
+                                           target="_blank">WFalumni</a>.
                                         All rights reserved.
                                     </p>
                                 </td>
